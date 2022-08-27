2af5ccb39333118cfe17f4ed44ed9c1b63f4e649
mvpc-11232 <mvp2612@hotmail.com>
2022-08-27 08:15:45 +0530

    Auto Push Commit

--- subnet.tf
+++ subnet.tf
@@ -1,8 +1,13 @@
 resource "aws_subnet" "main" {
   vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
+  cidr_block = ????????
 
   tags = {
     Name = "Main"
   }
 }
+
+
+
+
+#### The number of subntes to be created to be based on the numbers of SUBNET_CIDR's that we provide
