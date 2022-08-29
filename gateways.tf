d0dc60dd8163f47232d676d4e24f7930836c75f5
mvpc-11232 <mvp2612@hotmail.com>
2022-08-29 08:58:51 +0530

    Auto Push Commit

--- gateways.tf
+++ gateways.tf
@@ -16,7 +16,7 @@ resource "aws_eip" "ngw-eip" {
 # Nat gateway needs eip
 resource "aws_nat_gateway" "ngw" {
   allocation_id = aws_eip.ngw-eip.id
-  subnet_id     = aws_subnet.example.id
+  subnet_id     = aws_subnet.public.id
 
   tags = {
     Name = "gw NAT"
