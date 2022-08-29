5a1fae583d8aafa41a205cc63a76bd74c35ffac9
mvpc-11232 <mvp2612@hotmail.com>
2022-08-29 09:03:51 +0530

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -14,7 +14,7 @@ resource "aws_route_table" "public-route-table" {
   tags = {
     Name = "${var.ENV}-pub-route-table"
   }
-c:\Users\manoj\AppData\Local\Programs\Microsoft VS Code\resources\app\out\vs\code\electron-sandbox\workbench\workbench.html}
+}
 
 resource "aws_route_table_association" "public-rt-assoc" {
   count          = length(aws_subnet.public.*.id)
@@ -22,6 +22,33 @@ resource "aws_route_table_association" "public-rt-assoc" {
   route_table_id = aws_route_table.public-route-table.id
 }
 
+
+
+
+
+# Private
+resource "aws_route_table" "private-route-table" {
+  vpc_id     = aws_vpc.main.id
+
+  route {
+    cidr_block                = var.DEFAULT_VPC_CIDR
+    vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+  }
+  
+  route {
+    cidr_block                = "0.0.0.0/0"
+    gateway_id                = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "${var.ENV}-pub-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public-rt-assoc" {
+  count          = length(aws_subnet.public.*.id)
+  subnet_id      = element(aws_subnet.public.*.id, count.index)
+  route_table_id = aws_route_table.public-route-table.id
+}
 
 # resource "aws_route" "r" {
 #   route_table_id            = var.DEFAULT_VPC_RT
