21b832737e3a6f9aeecf7f93df4c3fe205015316
mvpc-11232 <mvp2612@hotmail.com>
2022-08-29 08:08:28 +0530

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -16,3 +16,11 @@ resource "aws_route_table_association" "rt-assoc" {
   subnet_id      = element(aws_subnet.main.*.id, count.index)
   route_table_id = aws_route_table.route-table.id
 }
+
+
+resource "aws_route" "r" {
+  route_table_id            = "rtb-4fbb3ac4"
+  destination_cidr_block    = "10.0.1.0/22"
+  vpc_peering_connection_id = "pcx-45ff3dc1"
+  depends_on                = [aws_route_table.testing]
+}
