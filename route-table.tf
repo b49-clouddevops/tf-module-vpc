d3c8637a96c37a7d71df38a6bd8564650b972127
mvpc-11232 <mvp2612@hotmail.com>
2022-08-29 09:06:05 +0530

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -46,10 +46,10 @@ resource "aws_route_table" "private-route-table" {
   depends_on  =  [aws_nat_gateway.ngw.id]
 }
 
-resource "aws_route_table_association" "public-rt-assoc" {
-  count          = length(aws_subnet.public.*.id)
-  subnet_id      = element(aws_subnet.public.*.id, count.index)
-  route_table_id = aws_route_table.public-route-table.id
+resource "aws_route_table_association" "private-rt-assoc" {
+  count          = length(aws_subnet.private.*.id)
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = aws_route_table.private-route-table.id
 }
 
 # resource "aws_route" "r" {
