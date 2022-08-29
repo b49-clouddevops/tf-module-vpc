8c5e678e35aa48d51af8d0ce4cf8419ca130ffd5
mvpc-11232 <mvp2612@hotmail.com>
2022-08-29 07:59:29 +0530

    Auto Push Commit

--- route-table.tf
+++ route-table.tf
@@ -14,6 +14,7 @@ resource "aws_route_table" "route-table" {
 rtb-043351e6418be34fc
 
 resource "aws_route_table_association" "rt-assoc" {
+  
   subnet_id      = aws_subnet.foo.id
   route_table_id = aws_route_table.bar.id
 }
