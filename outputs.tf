7c860fa15447b8cffb276110149a3cb72551902f
mvpc-11232 <mvp2612@hotmail.com>
2022-08-30 08:13:32 +0530

    Auto Push Commit

--- outputs.tf
+++ outputs.tf
@@ -6,7 +6,11 @@ output "VPC_CIDR" {
     value = aws_vpc.main.cidr_block
 }
 
-output "PRIVATE_SUBNET_CIDR"
+output "PRIVATE_SUBNET_CIDR" {
+    value = aws_subnet.private.*.id 
+}
 
 
-output 
+output "PUBLIC_SUBNET_CIDR" {
+    value = aws_subnet.public.*.id 
+}
