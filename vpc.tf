3b7c3b24280622d17db68f679452feca3bab2348
mvpc-11232 <mvp2612@hotmail.com>
2022-09-03 08:07:13 +0530

    Auto Push Commit

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block           = var.VPC_CIDR
-  enable_dns_hostnames = true
+  enable_dns_hostnames = true    # make to true, to enable DNS Translations in the VPC
+  
 
   tags = {
     Name = "${var.ENV}-vpc"
